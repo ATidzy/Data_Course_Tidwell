f5b21d7e6b4fe168a858198269080f726bd28215
alyssa <alyssaktidwell@gmail.com>
2019-10-22 00:32:03 -0600

    exam2

--- Assignments/Exam_2/Exam_2.R
+++ Assignments/Exam_2/Exam_2.R
@@ -34,27 +34,72 @@ mean_sq_err <- c((mod1a = (mean(mod1$residuals)^2)),
 names(mean_sq_err) <- c("mod1","mod2","mod3")
 bestmodel <- which(mean_sq_err == min(mean_sq_err))
 bestmodel <- get(names(bestmodel))
-
+bestmodel
 
 atmo<-atmo %>% gather_predictions(mod1,mod2,mod3)
 
-
+jpeg("./Tidwell_exam2_plot2.jpeg")
 ggplot(atmo,aes(x=Precip,color=model)) +
   geom_point(aes(y=Diversity),color="Black") +
   geom_point(aes(y=pred),alpha=.2) +
   facet_wrap(~model)
+dev.off()
+
 
 hyp <- read.csv("./hyp_data.csv")
-
 glimpse(hyp)
 
-hyp$mod1preds <- predict(mod1,newdata = hyp)
+hyp = add_predictions(hyp, mod1, var = "pred1")
+hyp = add_predictions(hyp, mod2, var = "pred2") 
+hyp = add_predictions(hyp, mod3, var = "pred1")
+
+#I still cn't figure out how to merge the different col lengths.
+#need more time with that.
+
+ggplot(hyp,aes(Precip,))+
+  geom_point()
+  geom_point(aes(y=atmo$Diversity), color="Black")+
+  geom_point(aes(y=hyp$mod1preds),color="Red")
 
 
-atmo <- read.csv("./atmosphere.csv")
+s <- summary(mod1)
+s2 <- summary(mod2)
+s3 <- summary(mod3)
+capture.output(s, file = "model_summary1.txt")
+capture.output(s2, file = "model_summary2.txt")
+capture.output(s3, file = "model_summary3.txt")
 
-merge.data.frame(atmo,hyp,)
 
-ggplot(atmo,aes(Precip, col=model))+
-  geom_point(aes(y=Diversity), color="Black")+
-  geom_point(aes(y=mod1preds),color="Red")
+#you asked for a test of the atmosphere data but I couldn't divide an
+#odd number of elements by 2 so you get turrbl hyp data.  
+rows = sample(nrow(hyp), replace = FALSE,nrow(hyp)/2)
+modhyp = hyp[rows,]
+crosshyp = hyp[-rows,]
+
+
+training = caret::createDataPartition(hyp,.5)
+training = (unlist(training))
+
+train <-hyp[training,]
+test <- hyp[-training,]
+
+
+trainingmodel <- lm(Aerosol_Density~Precip, data = train)
+trainingpreds <- add_predictions(test, trainingmodel)
+testingmodel <- lm(Aerosol_Density~Precip, data = test)
+
+testingpreds <- add_predictions(crosshyp, testingmodel,
+                model = testingmodel, var = "predz")
+#there is an error here that would be avoided with a better df.
+#so this plot doesn't work but it would otherwise
+ggplot(test,aes(Aerosol_Density,Precip)) +
+  geom_point()+
+  geom_smooth(method="lm", se=FALSE, alpha=.05, color="Pink") +
+  geom_point(aes(y=hyp$newcol),color="Purple")
+
+
+
+hypmod <- lm(Aerosol_Density~Precip, data =hyp)
+hyp$predz <- predict(hypmod)
+mean(abs(hyp$Aerosol_Density - hyp$predz))
+mean(abs(test$Aerosol_Density - test$predz))
